bb0500505007493ad57a386a7a122e929122c60f
Chimaobi <ps3hdguy@gmail.com>
2025-04-02 11:22:10 +0100

    version 1.07

--- Frontend/api.js
+++ Frontend/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL =
-  import.meta.env.NODE_ENV === "production"
+  import.meta.env.NODE_ENV == "production"
     ? import.meta.env.VITE_PROD_API_URL
     : import.meta.env.VITE_DEV_API_URL;
 

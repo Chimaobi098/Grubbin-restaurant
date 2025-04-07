75e8e65313e338d194e447a1e430814d04675d3d
Chimaobi <ps3hdguy@gmail.com>
2025-04-07 11:25:09 +0100

    bug fixes

--- Frontend/src/layout.jsx
+++ Frontend/src/layout.jsx
@@ -40,14 +40,10 @@ const Layout = () => {
     }
   }
 
-  // If still loading, render only the loading screen.
-  if (loading) {
-    return <LoadingScreen />;
-  }
-
   return (
     <>
       <ScrollRestoration />
+      {loading && <LoadingScreen />}
       <div className={styles.mainBodyContainer}>
         <header className={styles.header}>
           <div className={styles.companyDetails}>
